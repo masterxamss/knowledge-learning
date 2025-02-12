1fd737f13ad189f764e91e32927ccab4dbb128db
Tiago Machado <tiago.machado.1987@gmail.com>
2025-02-12 19:06:49 +0100

    add configs

---
+++ src/Security/UserChecker.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Security;
+
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserChecker implements UserCheckerInterface
+{
+    public function checkPreAuth(UserInterface $user): void
+    {
+        if (!$user instanceof \App\Entity\User) {
+            return;
+        }
+
+        if (!$user->getIsVerified()) {
+            throw new CustomUserMessageAuthenticationException('Votre compte n\'a pas encore été vérifié. Vérifiez votre e-mail.');
+        }
+
+
+        if (!$user->getActive()) {
+            throw new CustomUserMessageAuthenticationException('Votre compte a été suspendu. Contactez un administrateur.');
+        }
+    }
+
+    public function checkPostAuth(UserInterface $user): void {}
+}

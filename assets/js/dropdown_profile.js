74be50a211f85719f4d990bb62b70a4074aff6ec
Tiago Machado <tiago.machado.1987@gmail.com>
2025-02-15 23:17:56 +0100

    add script dropdown menu user profile

---
+++ assets/js/dropdown_profile.js
@@ -0,0 +1,28 @@
+document.addEventListener("turbo:load", () => {
+  let timeout;
+  const navItem = document.querySelector(".menu-item_profile");
+  const dropdown = document.querySelector(".menu-item_profile__dropdown");
+
+  if (navItem && dropdown) {
+    navItem.addEventListener("mouseenter", () => {
+      clearTimeout(timeout);
+      dropdown.style.display = "block";
+    });
+
+    navItem.addEventListener("mouseleave", () => {
+      timeout = setTimeout(() => {
+        dropdown.style.display = "none";
+      }, 200);
+    });
+
+    dropdown.addEventListener("mouseenter", () => {
+      clearTimeout(timeout);
+    });
+
+    dropdown.addEventListener("mouseleave", () => {
+      timeout = setTimeout(() => {
+        dropdown.style.display = "none";
+      }, 200);
+    });
+  }
+});

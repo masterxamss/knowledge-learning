70df88bd782a30c212cd89d27030e6922b0a1e4d
Tiago Machado <tiago.machado.1987@gmail.com>
2025-02-15 23:19:11 +0100

    formatting

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -9,16 +9,16 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
-    #[Route('/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
-    {
-        $error = $authenticationUtils->getLastAuthenticationError();
+  #[Route('/login', name: 'app_login')]
+  public function login(AuthenticationUtils $authenticationUtils): Response
+  {
+    $error = $authenticationUtils->getLastAuthenticationError();
 
-        $lastUsername = $authenticationUtils->getLastUsername();
+    $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('login/login.html.twig', [
-            'last_username' => $lastUsername,
-            'error' => $error
-        ]);
-    }
+    return $this->render('login/login.html.twig', [
+      'last_username' => $lastUsername,
+      'error' => $error
+    ]);
+  }
 }
